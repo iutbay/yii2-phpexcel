071d30bb3d2f65dff7915943fc37b2404192ae80
Kevin LEVRON <kevin.levron@univ-pau.fr>
2016-09-13 11:11:57 +0200

    Update PHPExcelResponseFormatter.php

--- PHPExcelResponseFormatter.php
+++ PHPExcelResponseFormatter.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace iutbay\yii2phpexcel;
+
+use Yii;
+use yii\base\Component;
+use yii\web\ResponseFormatterInterface;
+use yii\helpers\ArrayHelper;
+
+/**
+ * PHPExcel response formatter.
+ * 
+ * @author Kevin LEVRON <kevin.levron@gmail.com>
+ */
+class PHPExcelResponseFormatter extends Component implements ResponseFormatterInterface
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function format($response)
+    {
+    }
+
+}
