8c4bdd1e3347aae355cb35946f795159f4a12a01
Kevin LEVRON <kevin.levron@univ-pau.fr>
2016-09-13 11:51:08 +0200

    Update PHPExcelResponseFormatter.php

--- PHPExcelResponseFormatter.php
+++ PHPExcelResponseFormatter.php
@@ -20,6 +20,39 @@ class PHPExcelResponseFormatter extends Component implements ResponseFormatterIn
      */
     public function format($response)
     {
+        switch ($response->format) {
+            case 'xls' :
+                $this->formatXLS($response);
+                break;
+            default :
+                $this->formatCSV($response);
+                break;
+        }
+    }
+
+    /**
+     * @param \yii\web\Response $response
+     */
+    public function formatXLS($response)
+    {
+        $phpexcel = new \PHPExcel();
+		$phpexcel->setActiveSheetIndex(0);
+
+        $phpexcel->getActiveSheet()->fromArray($response->data['data']);
+        $filename = ArrayHelper::getValue($response->data, 'filename', 'data').'.xlsx';
+
+        $writer = new \PHPExcel_Writer_Excel2007($phpexcel);
+		header('Content-type: application/vnd.ms-excel');
+		header('Content-Disposition: attachment; filename="'.$filename.'"');
+		$writer->save('php://output');
+    }
+
+    /**
+     * @param \yii\web\Response $response
+     */
+    public function formatCSV($response)
+    {
+        
     }
 
 }
